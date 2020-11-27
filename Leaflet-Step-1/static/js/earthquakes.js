92e021327ed00507a5ef7e6c6048b48c7bd082c7
Kaylon <kcyoung@tiksdd.com>
2020-11-27 13:10:49 -0600

    Added partial code for legend.

--- Leaflet-Step-1/static/js/earthquakes.js
+++ Leaflet-Step-1/static/js/earthquakes.js
@@ -1,3 +1,15 @@
+function getColor(depth){
+    return depth > 90 ? '#f03535' :
+           depth > 70 ? '#f27227' :
+           depth > 50 ? '#f6ac17' :
+           depth > 30 ? '#f0de33' :
+           depth > 10 ? '#a1f64c' :
+                        '#3dc809' ;
+           
+}
+
+
+
 var myMap = L.map("map", {
     center: [39.8283, -98.5795],
     zoom: 4
@@ -37,43 +49,65 @@ var myMap = L.map("map", {
             console.log("depth before switch", depth)
             
 
-            if (depth > 90){
-                var color = "#f03535"
-            }
-            else if (depth > 70){
-                var color = "#f27227"
-            }
-            else if (depth > 50){
-                var color = "#f6ac17"
-            }
-            else if (depth > 30){
-                var color = "#f0de33"
-            }
-            else if (depth > 10){
-                var color = "#a1f64c"
-            }
-            else {
-                var color = "#3dc809"
-            }
+            // if (depth > 90){
+            //     var color = "#f03535"
+            // }
+            // else if (depth > 70){
+            //     var color = "#f27227"
+            // }
+            // else if (depth > 50){
+            //     var color = "#f6ac17"
+            // }
+            // else if (depth > 30){
+            //     var color = "#f0de33"
+            // }
+            // else if (depth > 10){
+            //     var color = "#a1f64c"
+            // }
+            // else {
+            //     var color = "#3dc809"
+            // }
 
             console.log("depth", depth)
-            console.log("color", color)
+            // console.log("color", color)
 
 
             var myCircle = L.circle([geometry.coordinates[1], geometry.coordinates[0]],{
                 radius: magnitude*10000,
                 color: 'black',
-                fillColor: color,
+                fillColor: getColor(geometry.coordinates[2]),
                 fillOpacity: .75,
                 weight: 1
-            }).bindPopup(`<h1>${properties.title}</h1> <hr> <h3>Status: ${properties.status}</h3>`)
+            }).bindPopup(`<h3>${properties.title}</h3> <hr> 
+            <h5>Status: ${properties.status}<br>
+            Earthquake Info: <a href = ${properties.url}>Detailed Earthquake Info</a><br>
+            </h5> `)
             .addTo(myMap);
-           
-           
+        }
+    });
 
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (myMap) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+        depthArray = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < depthArray.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(depthArray[i] + 1) + '"></i> ' +
+                depthArray[i] + (depthArray[i + 1] ? '&ndash;' + depthArray[i + 1] + '<br>' : '+');
         }
 
-    });
+        return div;
+    };
+
+    legend.addTo(myMap)
+
+   
+
+    // legend.addTo(map);
   
   });
   
